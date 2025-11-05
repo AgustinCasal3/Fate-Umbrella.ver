41d6bbc6ddc9ba0f52a910ee1dfe8ede33bfeaf7
Agustin Casal <agustincasal@isft220.edu.ar>
2025-11-05 08:51:39 -0300

    04-11 19:00 Agus

--- 0 Fate Umbrella.ver Web/Fate Umbrella.ver/src/instances/06 empresa/Empresa.tsx
+++ 0 Fate Umbrella.ver Web/Fate Umbrella.ver/src/instances/06 empresa/Empresa.tsx
@@ -15,114 +15,116 @@ export function Empresa() {
                     <img src="../../../imgs/Empresa/CunnyLogo.png" alt="Logo" />
                 </div>
 
-                <div className="separadorBlanco">
-                    <div className="lineaBlanca"></div>
-                </div>
-
                 <div className="integrantesEmpresa">
-                    <h2>Nuestros Owners</h2>
+                    <h2>
+                        <img src="../../../imgs/Empresa/SeparadorDiv.png" alt="" />
+                            Nuestros fundadores 
+                        <img src="../../../imgs/Empresa/SeparadorDiv.png" alt="" className='invertidoHorizontal'/>
+                    </h2>
                     <div className="ownersEmpresa">
-                        {/* Componente owner */}
-                        <div className="owner">
-                            <a href="https://www.youtube.com/@Aquwus" target='_blank'>
-                                <img src="../../../imgs/Empresa/Owners/Aquwus.png" alt="Foto de Aquwus" />
-                            </a>
-                            <span><a href="https://www.youtube.com/@Aquwus" target='_blank'>Aquwus</a></span>
-                        </div>
-                        <div className="owner">
-                            <a href="https://github.com/izruru" target='_blank'>
-                                <img src="../../../imgs/Empresa/Owners/Izuru.png" alt="Foto de Izuru" />
-                            </a>
-                            <span><a href="https://github.com/izruru" target='_blank'>Izuru</a></span>
+                        <div className="contenedorOwnersEmpresa">
+                            {/* Componente owner */}
+                            <div className="owner">
+                                <a href="https://www.youtube.com/@Aquwus" target='_blank'>
+                                    <img src="../../../imgs/Empresa/Owners/Aquwus.png" alt="Foto de Aquwus" />
+                                </a>
+                                <span><a href="https://www.youtube.com/@Aquwus" target='_blank'>Aquwus</a></span>
+                            </div>
+                            <div className="owner">
+                                <a href="https://github.com/izruru" target='_blank'>
+                                    <img src="../../../imgs/Empresa/Owners/Izuru.png" alt="Foto de Izuru" />
+                                </a>
+                                <span><a href="https://github.com/izruru" target='_blank'>Izuru</a></span>
+                            </div>
                         </div>
                     </div>
 
-                    <div className="separadorBlanco">
-                        <div className="lineaBlanca"></div>
-                    </div>
-
-                    <h2>Nuestros developers</h2>
+                    <h2>
+                        <img src="../../../imgs/Empresa/SeparadorDiv.png" alt="" />
+                            Nuestro equIpo
+                        <img src="../../../imgs/Empresa/SeparadorDiv.png" alt="" className='invertidoHorizontal'/>
+                    </h2>
                     <div className="empleadosEmpresa">
                         {/* Componente empleado */}
                         <div className="empleado">
-                            <a href="">
+                            <a href="https://danbooru.donmai.us/posts/7704321?q=possummachine+kokona_%28blue_archive%29+" target='_blank'>
                                 <img src="../../../imgs/Empresa/Workers/Kokona.png" alt="Foto de Kokona" />
                             </a>
                             <span><a href="">Kokona</a></span>
                         </div>
 
                         <div className="empleado">
-                            <a href="">
+                            <a href="" target='_blank'>
                                 <img src="../../../imgs/Empresa/Workers/Kisaki.png" alt="Foto de Kisaki" />
                             </a>
                             <span><a href="">Kisaki</a></span>
                         </div>
 
                         <div className="empleado">
-                            <a href="">
+                            <a href="" target='_blank'>
                                 <img src="../../../imgs/Empresa/Workers/Arona.png" alt="Foto de Arona" />
                             </a>
                             <span><a href="">Arona</a></span>
                         </div>
 
                         <div className="empleado">
-                            <a href="">
+                            <a href="" target='_blank'>
                                 <img src="../../../imgs/Empresa/Workers/Ibuki.png" alt="Foto de Ibuki" />
                             </a>
                             <span><a href="">Ibuki</a></span>
                         </div>
 
                         <div className="empleado">
-                            <a href="">
+                            <a href="" target='_blank'>
                                 <img src="../../../imgs/Empresa/Workers/Hoshino.png" alt="Foto de Hoshino" />
                             </a>
                             <span><a href="">Hoshino</a></span>
                         </div>
 
                         <div className="empleado">
-                            <a href="">
+                            <a href="" target='_blank'>
                                 <img src="../../../imgs/Empresa/Workers/Shiroko.png" alt="Foto de Shiroko" />
                             </a>
                             <span><a href="">Shiroko</a></span>
                         </div>
 
                         <div className="empleado">
-                            <a href="">
+                            <a href="" target='_blank'>
                                 <img src="../../../imgs/Empresa/Workers/Arisu.png" alt="Foto de Arisu" />
                             </a>
                             <span><a href="">Arisu</a></span>
                         </div>
 
                         <div className="empleado">
-                            <a href="">
+                            <a href="" target='_blank'>
                                 <img src="../../../imgs/Empresa/Workers/Serika.png" alt="Foto de Serika" />
                             </a>
                             <span><a href="">Serika</a></span>
                         </div>
 
                         <div className="empleado">
-                            <a href="">
+                            <a href="" target='_blank'>
                                 <img src="../../../imgs/Empresa/Workers/Hikari.png" alt="Foto de Hikari" />
                             </a>
                             <span><a href="">Hikari</a></span>
                         </div>
 
                         <div className="empleado">
-                            <a href="">
+                            <a href="" target='_blank'>
                                 <img src="../../../imgs/Empresa/Workers/Midori.png" alt="Foto de Midori" />
                             </a>
                             <span><a href="">Midori</a></span>
                         </div>
 
                         <div className="empleado">
-                            <a href="">
+                            <a href="" target='_blank'>
                                 <img src="../../../imgs/Empresa/Workers/Momoi.png" alt="Foto de Momoi" />
                             </a>
                             <span><a href="">Momoi</a></span>
                         </div>
 
                         <div className="empleado">
-                            <a href="">
+                            <a href="" target='_blank'>
                                 <img src="../../../imgs/Empresa/Workers/Seia.png" alt="Foto de Seia" />
                             </a>
                             <span><a href="">Seia</a></span>
@@ -146,12 +148,16 @@ export function Empresa() {
                         sino que también deje una huella en cada jugador.
                     </p>
                 </div>
+                <img src="../../../imgs/Empresa/SaberChibi.png" alt="" />
             </section>
 
             <section className="misionVision">
                 <div className="contenedorMisionVision">
                     <div className="mision">
                         <h2>MISION</h2>
+                        <div className="imgContainer">
+                            <img src="../../../imgs/Empresa/SeparadorTexto2.png" alt="" />
+                        </div>
                         <p>
                             En XXXX tenemos el objetivo principal de brindar software de entretenimiento esencial, 
                             principalmente videojuegos, que inspiren a las personas a crear y encontrar aquello que les permita perseguir sus sueños. 
@@ -162,6 +168,9 @@ export function Empresa() {
                     <div className="separadorMV"></div>
                     <div className="vision">
                         <h2>VISION</h2>
+                        <div className="imgContainer">
+                            <img src="../../../imgs/Empresa/SeparadorTexto2.png" alt="" />
+                        </div>
                         <p>
                             En XXXX buscamos llegar a usuarios en todo el planeta, expandir nuestro software a otras plataformas 
                             (comenzando por PC, continuando con mobile, consolas y VR) y convertirnos en una empresa de desarrollo
@@ -174,18 +183,46 @@ export function Empresa() {
             <section className="valores">
                 <div className="contenedorValores">
                     <h2>Nuestros Valores</h2>
-                    <p>Honestidad y transparencia: Comunicarnos de manera clara, sincera y directa con nuestros clientes y empleados.</p>
-                    <p>Innovación: Apostar a lo nuevo, a lo experimental y a lo disruptivo para diferenciarnos del resto de las empresas.</p>
-                    <p>Calidad: Asegurarnos de que cada producto cumpla con altos estándares técnicos y de diseño.</p>
-                    <p>Respeto por el cliente: Escuchar sus necesidades y ponerlas en el centro de cada proyecto.</p>
-                    <p>Trabajo en equipo: Fomentar la colaboración interna como clave del éxito.</p>
+
+                    <div className="contenedorTarjetas">
+                        <div className="tarjetaValor">
+                            <h3>Honestidad y transparencia</h3>
+                            <p>Comunicarnos de manera clara, sincera y directa con nuestros clientes y empleados.</p>
+                        </div>
+                        <div className="tarjetaValor">
+                            <h3>Innovación</h3>
+                            <p>Apostar a lo nuevo, a lo experimental y a lo disruptivo para diferenciarnos del resto de las empresas.</p>
+                        </div>
+                        <div className="tarjetaValor">
+                            <h3>Calidad</h3>
+                            <p>Asegurarnos de que cada producto cumpla con altos estándares técnicos y de diseño.</p>
+                        </div>
+                    </div>
+                    <div className="contenedorTarjetas">
+                        <div className="tarjetaValor">
+                            <h3>Respeto por el cliente</h3>
+                            <p>Escuchar sus necesidades y ponerlas en el centro de cada proyecto.</p>
+                        </div>
+                        <div className="tarjetaValor">
+                            <h3>Trabajo en equipo</h3>
+                            <p>Fomentar la colaboración interna como clave del éxito.</p>
+                        </div>
+                    </div>
                 </div>
             </section>
 
             <section className="reclutamiento">
                 <div className="contenedorReclutamiento">
+                    <div className="contenedorImgReclutamiento">
+                        <img src="../../../imgs/Empresa/SeparadorDiv2.png" alt="" />
+                    </div>
                     <h3>En CunnyWorks siempre estamos en busca de nuevos taletos</h3>
-                    <button>Cuentanos tu historia</button>
+                    <div className="contenedorButtonReclutamiento">
+                        <button>Cuentanos tu historia</button>
+                    </div>
+                    <div className="contenedorImgReclutamiento">
+                        <img src="../../../imgs/Empresa/SeparadorDiv2.png" alt="" className='invertidoVertical'/>
+                    </div>
                 </div>
             </section>
 
